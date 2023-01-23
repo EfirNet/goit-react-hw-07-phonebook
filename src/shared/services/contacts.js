fe59e6077cd6eae1ece45f8c9125352d8bf4a4bc
efirnet <efirnet@gmail.com>
2023-01-23 18:41:07 +0200

    fix

--- src/shared/services/contacts.js
+++ src/shared/services/contacts.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
-const instsance = axios.create({
+const instance = axios.create({
   baseURL: "https://63caa4aa4f53a0042027c558.mockapi.io/contacts"
 });
 
 export const fetchContacts = async () => {
-  const { data } = await instsance.get("/");
+  const { data } = await instance.get("/");
   return data;
 }
 
 export const addContact = async (data) => {
-  const { data: result } = await instsance.post("/", data);
+  const { data: result } = await instance.post("/", data);
   return result;
 }
 
 export const deleteContact = async (id) => {
-  const { data } = await instsance.delete(`/${id}`);
+  const { data } = await instance.delete(`/${id}`);
   return data;
 }
